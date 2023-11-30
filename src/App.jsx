60d1f0ed5591808a882427364a15df7a1c7ae213
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-30 08:55:54 +0200

    app init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { AppWrapper } from './components/Helpers/Components.styled';
+import api from 'api/api';
+import { AppWrapper } from 'components/Helpers/Components.styled';
+import { InfoSection } from 'components/InfoSection/InfoSection';
+import { SearchForm } from 'components/SearchForm/SearchForm';
+// import { useState } from 'react';
 
 export const App = () => {
-  return <AppWrapper>React homework template</AppWrapper>;
+  // const [weatherInfo, setWeatherInfo] = useState([]);
+
+  return (
+    <AppWrapper>
+      <SearchForm />
+      <InfoSection />
+    </AppWrapper>
+  );
 };
