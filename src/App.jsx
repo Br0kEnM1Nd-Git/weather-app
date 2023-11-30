6b501cb050455fcd3e6c43cdf0ec83171330da32
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-30 09:22:56 +0200

    upd app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import api from 'api/api';
 import { AppWrapper } from 'components/Helpers/Components.styled';
 import { InfoSection } from 'components/InfoSection/InfoSection';
 import { SearchForm } from 'components/SearchForm/SearchForm';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 export const App = () => {
-  // const [weatherInfo, setWeatherInfo] = useState([]);
+  const [weatherInfo, setWeatherInfo] = useState([]);
+
+  const handleSubmit = weather => {
+    console.log(weather);
+    setWeatherInfo(weather);
+  };
 
   return (
     <AppWrapper>
-      <SearchForm />
-      <InfoSection />
+      <SearchForm onSubmit={handleSubmit} />
+      <InfoSection weatherInfo={weatherInfo} />
     </AppWrapper>
   );
 };
