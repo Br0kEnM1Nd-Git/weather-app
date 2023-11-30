f7a341a11ef53c6ff97a8ca72d845a648bb8b036
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-30 12:53:01 +0200

    add filter by date

--- src/components/Helpers/paginate.js
+++ src/components/Helpers/paginate.js
@@ -1,7 +1,21 @@
+// const paginate = weatherList => {
+//   const batches = [];
+//   for (let i = 0; i < 5; i++) {
+//     batches.push(weatherList.splice(0, 8));
+//   }
+//   return batches;
+// };
+
 const paginate = weatherList => {
   const batches = [];
   for (let i = 0; i < 5; i++) {
-    batches.push(weatherList.splice(0, 8));
+    const currDate = weatherList[0].date.split(' ')[0];
+    const filteredWeather = weatherList.filter(el => {
+      return el.date.split(' ')[0] === currDate;
+    });
+    batches.push(filteredWeather);
+    const length = filteredWeather.length;
+    weatherList.splice(0, length);
   }
   return batches;
 };
